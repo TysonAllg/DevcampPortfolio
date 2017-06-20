a3b1a0259c91951c667b1329279bced58872cfa8
Alpine Devcamp <alpine@devcamp.com>
2017-06-20 14:20:38 +0000

    Portfolio Feature-Seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,41 @@
-10.times do |blog|
-      Blog.create!(
-        title: "My Blog Post #{blog}"  
-        body:  "Random crap.")
+Blog.create!(
+    title: “My Blog Post #{blog}”,
+
 end
 
-puts "10 blog posts created"
+10 times do |blog|
+    puts blog
+end
+
+Blog.create!(
+    title: “My Blog Post #{blog}”,
+    body: “Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+    eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+    voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+    incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam 
+    corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate 
+    velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?”
+)
+end
 
 5.times do |skill|
-    Skill.create!(
-        title: "rails #{skill}"
-        percent_used:15
-        )
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
 end
 
+puts "10 blog posts created" 
 puts "5 skills created"
 
 9.times do |portfolio_item|
-    Portfolio.create!(
-       
-      title: "Portfolio title #{portfolio_item}",
-      subtitle: "My Service",
-      body: "dit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut",
-      main_image: "http://via.placeholder.com/650x650",
-      thumb_image: "http://via.placeholder.com/350x350",
-        
-        )
-puts "9 portfolio items created"
+  Portfolio.create!(
+    title:"Portfolio title: #{portfolio_item}”,
+    subtitle:Content,
+    body:Body,
+    main_image:http://via.placeholder.com/650x650, 
+    thumb_image:http://via.placeholder.com/350x350
+    )
 end
+
