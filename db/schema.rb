8b925a528b8f25f7d8105b87bea54080fe1adcd0
Alpine Devcamp <alpine@devcamp.com>
2017-06-22 15:51:09 +0000

    enums

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621172015) do
+ActiveRecord::Schema.define(version: 20170622150318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "slug"
-    t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "slug"
+    t.integer  "status",     default: 0
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
